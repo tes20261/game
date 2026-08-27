ee4ca72a9c608cf40d369090ff60f808611555b0
Ederson Torresini <etorresini@ifsc.edu.br>
2026-08-26 21:44:58 -0300

    Formatar mais com Prettier

--- .proxyrc.js
+++ .proxyrc.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
 module.exports = function (app) {
-  app.use('/assets', express.static(path.join(__dirname, 'public/assets/')))
-}
+  app.use("/assets", express.static(path.join(__dirname, "public/assets/")));
+};
